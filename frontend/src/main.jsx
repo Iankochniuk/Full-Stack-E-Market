225f09d5249b8097d16296a014aa5973dab14d7a
Ian Kochniuk <ianik1037@gmail.com>
2026-06-19 20:57:10 -0300

    reajuste en BroserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,9 +9,9 @@ import AuthProvider from "./context/AuthContext";
 createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <CartProvider>
-      <BBrowserRouter>
+      <BrowserRouter>
         <App />
-      </BBrowserRouter>
+      </BrowserRouter>
     </CartProvider>
   </AuthProvider>,
 );
